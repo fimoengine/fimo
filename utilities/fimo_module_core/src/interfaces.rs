6fcf0ac54acfe6a9f3bed1b41d1cd89307b3a643
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2022-01-09 22:31:05 +0100

    Add utilities for casting interfaces

--- utilities/fimo_module_core/src/interfaces.rs
+++ utilities/fimo_module_core/src/interfaces.rs
@@ -1,7 +1,7 @@
-use crate::{ModuleInfo, ModuleInterfaceDescriptor, PathChar, Result};
-use fimo_ffi::span::SpanInner;
+use crate::{Error, ErrorKind, ModuleInfo, ModuleInterfaceDescriptor, PathChar, Result};
+use fimo_ffi::object::ObjectWrapper;
 use fimo_ffi::vtable::BaseInterface;
-use fimo_ffi::{fimo_object, fimo_vtable, ObjArc, Object};
+use fimo_ffi::{fimo_object, fimo_vtable, ObjArc, Object, Optional, SpanInner, StrInner};
 use fimo_version_core::Version;
 use std::path::{Path, PathBuf};
 
@@ -336,6 +336,16 @@ fimo_vtable! {
     }
 }
 
+/// Marker trait for interfaces.
+pub trait FimoInterface {
+    /// Name of the interface.
+    const NAME: &'static str;
+    /// Version of the interface.
+    const VERSION: Version;
+    /// Required extensions.
+    const EXTENSIONS: &'static [&'static str];
+}
+
 fimo_object! {
     /// A type-erased module interface.
     pub struct ModuleInterface<vtable = ModuleInterfaceVTable>;
@@ -361,11 +371,74 @@ impl ModuleInterface {
         unsafe { (vtable.version)(ptr) }
     }
 
+    /// Fetches an extension from the interface.
+    #[inline]
+    pub fn extension(&self, name: &str) -> Option<&Object<BaseInterface>> {
+        let (ptr, vtable) = self.into_raw_parts();
+        unsafe {
+            (vtable.extension)(ptr, name.into())
+                .into_rust()
+                .map(|e| &*e)
+        }
+    }
+
     /// Fetches the parent instance.
     #[inline]
     pub fn instance(&self) -> ObjArc<ModuleInstance> {
         let (ptr, vtable) = self.into_raw_parts();
         unsafe { (vtable.instance)(ptr) }
+    }
+
+    /// Tries to downcast the interface.
+    #[inline]
+    pub fn try_downcast<T: ObjectWrapper + FimoInterface + ?Sized>(
+        &self,
+    ) -> std::result::Result<&T, Error> {
+        // before downcasting we must ensure that the versions
+        // are compatible and that all extensions are present.
+        if T::VERSION.is_compatible(&self.version()) {
+            return Err(Error::new(
+                ErrorKind::InvalidArgument,
+                format!(
+                    "interface version incompatible, requested `{}`, available `{}`",
+                    T::VERSION,
+                    self.version()
+                ),
+            ));
+        }
+
+        for &ext in T::EXTENSIONS {
+            if self.extension(ext).is_none() {
+                return Err(Error::new(
+                    ErrorKind::InvalidArgument,
+                    format!("extension `{}` not found", ext),
+                ));
+            }
+        }
+
+        let inner = self.as_inner();
+        match inner.try_cast::<T::VTable>() {
+            Ok(i) => unsafe { Ok(&*T::from_object(i)) },
+            Err(e) => Err(Error::new(
+                ErrorKind::InvalidArgument,
+                format!(
+                    "interface type mismatch, requested `{}`, available `{}`",
+                    e.required, e.available
+                ),
+            )),
+        }
+    }
+
+    /// Tries to downcast the interface.
+    #[inline]
+    pub fn try_downcast_arc<T: ObjectWrapper + FimoInterface + ?Sized>(
+        i: ObjArc<ModuleInterface>,
+    ) -> std::result::Result<ObjArc<T>, Error> {
+        // the inner object always equals the original object, except for the different vtable.
+        // because of that we can simply perform the casting ourselves and rebuild the arc.
+        let inner = i.try_downcast::<T>()? as *const _;
+        let (_, alloc) = ObjArc::into_raw_parts(i);
+        unsafe { Ok(ObjArc::from_raw_parts(inner, alloc)) }
     }
 }
 
@@ -377,6 +450,8 @@ fimo_vtable! {
         pub inner: unsafe extern "C" fn(*const ()) -> &'static BaseInterface,
         /// Extracts the version of the implemented interface.
         pub version: unsafe extern "C" fn(*const ()) -> Version,
+        /// Fetches an extension from the interface.
+        pub extension: unsafe extern "C" fn(*const (), StrInner<false>) -> Optional<*const Object<BaseInterface>>,
         /// Fetches the parent instance.
         pub instance: unsafe extern "C" fn(*const ()) -> ObjArc<ModuleInstance>,
     }
