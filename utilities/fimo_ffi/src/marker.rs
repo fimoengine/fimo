705b952b04ab9e5ff4a6e90c58e91f874582648d
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2022-02-01 19:42:00 +0100

    Reexport markers

--- utilities/fimo_ffi/src/marker.rs
+++ utilities/fimo_ffi/src/marker.rs
@@ -2,35 +2,11 @@
 use crate::fimo_marker;
 use fimo_object::vtable::DefaultMarker;
 
+pub use fimo_object::vtable::{SendMarker, SendSyncMarker, SyncMarker};
+
 fimo_marker! {
     /// A marker which implements neither `Send` nor `Sync`.
     pub marker NoneMarker {
         _inner: DefaultMarker,
     }
-}
-
-fimo_marker! {
-    /// A marker which implements `Send`.
-    #![requires(Send)]
-    pub marker SendMarker {
-        _inner: DefaultMarker,
-    }
-}
-
-unsafe impl Send for SendMarker {}
-
-fimo_marker! {
-    /// A marker which implements `Sync`.
-    #![requires(Sync)]
-    pub marker SyncMarker {
-        _inner: DefaultMarker,
-    }
-}
-
-unsafe impl Sync for SyncMarker {}
-
-fimo_marker! {
-    /// A marker which implements both `Send` and `Sync`.
-    #![requires(Send, Sync)]
-    pub marker SendSyncMarker;
 }
