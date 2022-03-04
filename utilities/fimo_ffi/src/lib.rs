2958070a12d55bbd4a4276a47ad88a629ba30835
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2022-03-04 23:20:24 +0100

    Change span and str definitions

--- utilities/fimo_ffi/src/lib.rs
+++ utilities/fimo_ffi/src/lib.rs
@@ -6,17 +6,22 @@
     rustdoc::broken_intra_doc_links
 )]
 #![allow(incomplete_features)]
+#![feature(const_slice_from_raw_parts)]
 #![feature(const_fn_fn_ptr_basics)]
 #![feature(const_ptr_offset_from)]
 #![feature(const_fn_trait_bound)]
+#![feature(const_slice_ptr_len)]
 #![feature(alloc_layout_extra)]
+#![feature(const_trait_impl)]
 #![feature(try_reserve_kind)]
 #![feature(unboxed_closures)]
 #![feature(const_type_name)]
 #![feature(dropck_eyepatch)]
 #![feature(specialization)]
+#![feature(const_mut_refs)]
 #![feature(layout_for_ptr)]
 #![feature(allocator_api)]
+#![feature(const_convert)]
 #![feature(set_ptr_value)]
 #![feature(slice_ptr_len)]
 #![feature(cfg_sanitize)]
@@ -44,14 +49,14 @@ pub mod vec;
 
 pub use fimo_version_core as version;
 
-pub use crate::str::{ConstStr, MutStr, StrInner};
+pub use crate::str::{ConstStr, MutStr};
 pub use ffi_fn::FfiFn;
 pub use obj_arc::{ObjArc, ObjWeak};
 pub use obj_box::ObjBox;
 pub use optional::Optional;
 pub use ptr::DynObj;
 pub use result::Result;
-pub use span::{ConstSpan, MutSpan, SpanInner};
+pub use span::{ConstSpan, MutSpan};
 pub use string::String;
 pub use vec::Vec;
 pub use version::{ReleaseType, Version};
