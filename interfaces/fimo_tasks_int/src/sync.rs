4a6892c72d20f8b6e5742448ea9be4ed2588d486
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2022-04-06 13:19:24 +0200

    Add Barrier impl

--- interfaces/fimo_tasks_int/src/sync.rs
+++ interfaces/fimo_tasks_int/src/sync.rs
@@ -1,11 +1,13 @@
 //! Synchronization primitives
 //!
 //! This module provides Runtime-aware implementations
-//! of `Mutex`, `RWLock` and `Condvar`.
+//! of `Mutex`, `RWLock`, `Condvar` and `Barrier`.
 
+mod barrier;
 mod condvar;
 mod mutex;
 mod spin_wait;
 
+pub use barrier::Barrier;
 pub use condvar::Condvar;
 pub use mutex::{Mutex, MutexGuard};
--- modules/fimo_tasks/tests/sync/mod.rs
+++ modules/fimo_tasks/tests/sync/mod.rs
@@ -1,2 +1,3 @@
+mod barrier;
 mod condvar;
 mod mutex;
