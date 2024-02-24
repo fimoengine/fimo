740d8e65f3f3f784198504fc36a4626fe5c69bf2
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2024-02-24 02:14:26 +0100

    Replace __builtin_popcountg with its non-generic variants

--- ffi_library/fimo_std/include/fimo_std/impl/integers/count_ones.h
+++ ffi_library/fimo_std/include/fimo_std/impl/integers/count_ones.h
@@ -25,8 +25,9 @@ extern "C" {
  */
 static FIMO_INLINE_ALWAYS FimoU32 fimo_impl_count_ones_u8(FimoU8 v)
 {
-#if FIMO_HAS_BUILTIN(__builtin_popcountg)
-    return __builtin_popcountg(v);
+#if FIMO_HAS_BUILTIN(__builtin_popcount)
+    _Static_assert(sizeof(FimoU8) <= sizeof(unsigned int), "Size mismatch");
+    return __builtin_popcount(v);
 #elif defined(_MSC_VER) && (defined(_M_IX86) || defined(_M_X64))
     return __popcnt16(v);
 #elif defined(_MSC_VER) && (defined(_M_ARM) || defined(_M_ARM64))
@@ -44,8 +45,9 @@ static FIMO_INLINE_ALWAYS FimoU32 fimo_impl_count_ones_u8(FimoU8 v)
  */
 static FIMO_INLINE_ALWAYS FimoU32 fimo_impl_count_ones_u16(FimoU16 v)
 {
-#if FIMO_HAS_BUILTIN(__builtin_popcountg)
-    return __builtin_popcountg(v);
+#if FIMO_HAS_BUILTIN(__builtin_popcount)
+    _Static_assert(sizeof(FimoU16) <= sizeof(unsigned int), "Size mismatch");
+    return __builtin_popcount(v);
 #elif defined(_MSC_VER) && (defined(_M_IX86) || defined(_M_X64))
     return __popcnt16(v);
 #elif defined(_MSC_VER) && (defined(_M_ARM) || defined(_M_ARM64))
@@ -63,8 +65,9 @@ static FIMO_INLINE_ALWAYS FimoU32 fimo_impl_count_ones_u16(FimoU16 v)
  */
 static FIMO_INLINE_ALWAYS FimoU32 fimo_impl_count_ones_u32(FimoU32 v)
 {
-#if FIMO_HAS_BUILTIN(__builtin_popcountg)
-    return __builtin_popcountg(v);
+#if FIMO_HAS_BUILTIN(__builtin_popcount)
+    _Static_assert(sizeof(FimoU32) <= sizeof(unsigned int), "Size mismatch");
+    return __builtin_popcount(v);
 #elif defined(_MSC_VER) && (defined(_M_IX86) || defined(_M_X64))
     return __popcnt(v);
 #elif defined(_MSC_VER) && (defined(_M_ARM) || defined(_M_ARM64))
@@ -82,8 +85,9 @@ static FIMO_INLINE_ALWAYS FimoU32 fimo_impl_count_ones_u32(FimoU32 v)
  */
 static FIMO_INLINE_ALWAYS FimoU32 fimo_impl_count_ones_u64(FimoU64 v)
 {
-#if FIMO_HAS_BUILTIN(__builtin_popcountg)
-    return __builtin_popcountg(v);
+#if FIMO_HAS_BUILTIN(__builtin_popcountll)
+    _Static_assert(sizeof(FimoU64) <= sizeof(unsigned long long), "Size mismatch");
+    return __builtin_popcountll(v);
 #elif defined(_MSC_VER) && defined(_M_X64)
     return (FimoU32)__popcnt64(v);
 #elif defined(_MSC_VER) && (defined(_M_ARM) || defined(_M_ARM64))
