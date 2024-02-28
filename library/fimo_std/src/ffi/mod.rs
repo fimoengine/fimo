b599f8a3fafe48410f20ecad79abed06049586ab
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2024-02-28 11:07:34 +0100

    Add FFISharable

--- library/fimo_std/src/ffi/mod.rs
+++ library/fimo_std/src/ffi/mod.rs
@@ -15,3 +15,21 @@ pub trait FFITransferable<FfiType: Sized> {
     /// The caller must ensure to have the ownership of the ffi type.
     unsafe fn from_ffi(ffi: FfiType) -> Self;
 }
+
+/// Used to share ownership with and from a ffi interface.
+///
+/// The ownership of a type is shared by calling [`Self::share_to_ffi`] and
+/// is borrowed by calling [`Self::borrow_from_ffi`].
+pub trait FFISharable<FfiType: Sized> {
+    type BorrowedView<'a>: 'a;
+
+    /// Shares the value of a Rust type with a ffi type.
+    fn share_to_ffi(&self) -> FfiType;
+
+    /// Borrows the ownership of a ffi type.
+    ///
+    /// # Safety
+    ///
+    /// The caller must ensure that all invariants of the type are conserved.
+    unsafe fn borrow_from_ffi<'a>(ffi: FfiType) -> Self::BorrowedView<'a>;
+}
