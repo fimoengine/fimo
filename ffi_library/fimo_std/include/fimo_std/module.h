9e1445b4586b1ad3cf807c8143430f7b4834ac76
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2024-03-29 23:44:05 +0100

    Decouple the public module api from the private api

--- ffi_library/fimo_std/include/fimo_std/module.h
+++ ffi_library/fimo_std/include/fimo_std/module.h
@@ -10,35 +10,10 @@
 #include <fimo_std/error.h>
 #include <fimo_std/version.h>
 
+#include <fimo_std/impl/module.h>
+
 #ifdef __cplusplus
 extern "C" {
-#endif
-
-#ifdef _WIN32
-// With the MSVC we have no way to get the start and end of
-// a section, so we use three different sections. According
-// to the documentation, the linker orders the entries with
-// the same section prefix by the section name. Therefore,
-// we can get the same result by allocating all entries in
-// the middle section.
-#pragma section("fi_mod$a", read)
-#pragma section("fi_mod$u", read)
-#pragma section("fi_mod$z", read)
-
-/**
- * Name of the section where the modules will be stored to.
- */
-#define FIMO_MODULE_SECTION "fi_mod$u"
-#elif __APPLE__
-/**
- * Name of the section where the modules will be stored to.
- */
-#define FIMO_MODULE_SECTION "__DATA,__fimo_module"
-#else
-/**
- * Name of the section where the modules will be stored to.
- */
-#define FIMO_MODULE_SECTION "fimo_module"
 #endif
 
 /**
@@ -376,10 +351,10 @@ extern "C" {
 
 #ifdef _WIN32
 #define FIMO_MODULE_EXPORT_MODULE__(VAR)                                                                               \
-    __declspec(allocate(FIMO_MODULE_SECTION)) const FimoModuleExport *FIMO_VAR(VAR) = &VAR;
+    __declspec(allocate(FIMO_IMPL_MODULE_SECTION)) const FimoModuleExport *FIMO_VAR(VAR) = &VAR;
 #else
 #define FIMO_MODULE_EXPORT_MODULE__(VAR)                                                                               \
-    const FimoModuleExport *FIMO_VAR(VAR) __attribute__((used, section(FIMO_MODULE_SECTION))) = &VAR;
+    const FimoModuleExport *FIMO_VAR(VAR) __attribute__((used, section(FIMO_IMPL_MODULE_SECTION))) = &VAR;
 #endif
 
 #define FIMO_MODULE_EXPORT_MODULE_(VAR, NAME, DESC, AUTHOR, LICENSE, ...)                                              \
--- ffi_library/fimo_std/src/internal/module.c
+++ ffi_library/fimo_std/src/internal/module.c
@@ -27,32 +27,6 @@
 #define FIMO_MALLOC_ALIGNMENT _Alignof(max_align_t)
 #endif
 
-#ifdef _WIN32
-__declspec(allocate("fi_mod$a")) const FimoModuleExport *fimo_internal_modules_section_start = NULL;
-__declspec(allocate("fi_mod$z")) const FimoModuleExport *fimo_internal_modules_section_end = NULL;
-
-#define FIMO_INTERNAL_MODULES_SECTION_START fimo_internal_modules_section_start
-#define FIMO_INTERNAL_MODULES_SECTION_END fimo_internal_modules_section_end
-#elif __APPLE__
-// Allocate a dummy module to force the creation of the section symbols.
-const FimoModuleExport *fimo_internal_modules_dummy_module __attribute__((section(FIMO_MODULE_SECTION))) = NULL;
-
-extern const FimoModuleExport *fimo_internal_modules_section_start __asm("section$start$__DATA$__fimo_module");
-extern const FimoModuleExport *fimo_internal_modules_section_end __asm("section$end$__DATA$__fimo_module");
-
-#define FIMO_INTERNAL_MODULES_SECTION_START fimo_internal_modules_section_start
-#define FIMO_INTERNAL_MODULES_SECTION_END fimo_internal_modules_section_end
-#else
-// Allocate a dummy module to force the creation of the section symbols.
-const FimoModuleExport *fimo_internal_modules_dummy_module __attribute__((section(FIMO_MODULE_SECTION))) = NULL;
-
-extern const FimoModuleExport *__start_fimo_module;
-extern const FimoModuleExport *__stop_fimo_module;
-
-#define FIMO_INTERNAL_MODULES_SECTION_START __start_fimo_module
-#define FIMO_INTERNAL_MODULES_SECTION_END __stop_fimo_module
-#endif
-
 static void *malloc_(size_t size) { return fimo_malloc(size, NULL); }
 
 static void *realloc_(void *ptr, size_t size) {
@@ -1245,7 +1219,7 @@ static FimoError module_handle_new_local_(void (*export_iterator)(bool (*)(const
         goto get_module_path;
     }
 
-    const char *module_bin_path = export_iterator == fimo_internal_module_export_iterator ? NULL : dl_info.dli_fname;
+    const char *module_bin_path = export_iterator == fimo_impl_module_export_iterator ? NULL : dl_info.dli_fname;
     handle = dlopen(module_bin_path, RTLD_NOW | RTLD_LOCAL | RTLD_NOLOAD);
     if (handle == NULL) {
         error = FIMO_EUNKNOWN;
@@ -2901,7 +2875,7 @@ static FimoError fi_module_new_pseudo_(FimoInternalModuleContext *ctx, const cha
     FIMO_DEBUG_ASSERT(ctx && name && element)
     TRACE_(ctx, "name='%s', element='%p'", name, (void *)element)
     struct ModuleHandle_ *handle;
-    FimoError error = module_handle_new_local_(fimo_internal_module_export_iterator, &handle);
+    FimoError error = module_handle_new_local_(fimo_impl_module_export_iterator, &handle);
     if (FIMO_IS_ERROR(error)) {
         ERROR_SIMPLE_(ctx, error, "could not construct module handle")
         return error;
@@ -3670,27 +3644,6 @@ FimoError fimo_internal_trampoline_module_get_inner(void *ctx, const FimoModule 
 ///////////////////////////////////////////////////////////////////////
 //// Module Subsystem API
 ///////////////////////////////////////////////////////////////////////
-
-FIMO_INTERNAL_EXPORT
-void fimo_internal_module_export_iterator(const FimoInternalModuleInspector inspector, void *data) {
-    if (inspector == NULL) {
-        return;
-    }
-
-    const FimoModuleExport **start = &FIMO_INTERNAL_MODULES_SECTION_START;
-    const FimoModuleExport **end = &FIMO_INTERNAL_MODULES_SECTION_END;
-    for (const FimoModuleExport **it = start; it != end; it++) {
-        // Skip empty module declarations.
-        if ((*it) == NULL) {
-            continue;
-        }
-
-        // Pass the module to the inspection function.
-        if (!inspector(*it, data)) {
-            break;
-        }
-    }
-}
 
 FIMO_MUST_USE
 FimoError fimo_internal_module_init(FimoInternalModuleContext *ctx) {
