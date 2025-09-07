f06ab087fb72a68b7dbf74169f622dbe82ada78c
Gabriel Borrelli <borrelli.gabriel.pr@gmail.com>
2025-09-07 13:56:53 +0200

    fimo_tasks: Remove extra compilation step for boost context

--- modules/fimo_tasks/build.zig
+++ modules/fimo_tasks/build.zig
@@ -15,51 +15,6 @@ pub fn configure(builder: *build_internals.FimoBuild) void {
         },
     );
     const context_path = context_dep.path("src");
-    const context = b.addLibrary(.{
-        .linkage = .static,
-        .name = "context",
-        .root_module = b.createModule(.{
-            .target = builder.graph.target,
-            .optimize = builder.graph.optimize,
-        }),
-    });
-    switch (context.rootModuleTarget().cpu.arch) {
-        .aarch64 => switch (context.rootModuleTarget().os.tag) {
-            .windows => {
-                if (context.rootModuleTarget().abi == .msvc) {
-                    context.root_module.addAssemblyFile(context_path.path(b, "asm/jump_arm64_aapcs_pe_armasm.asm"));
-                    context.root_module.addAssemblyFile(context_path.path(b, "asm/ontop_arm64_aapcs_pe_armasm.asm"));
-                }
-            },
-            .macos, .ios, .watchos, .tvos, .visionos => {
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/jump_arm64_aapcs_macho_gas.S"));
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/make_arm64_aapcs_macho_gas.S"));
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/ontop_arm64_aapcs_macho_gas.S"));
-            },
-            else => {
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/jump_arm64_aapcs_elf_gas.S"));
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/make_arm64_aapcs_elf_gas.S"));
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/ontop_arm64_aapcs_elf_gas.S"));
-            },
-        },
-        .x86_64 => switch (context.rootModuleTarget().os.tag) {
-            .windows => {
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/jump_x86_64_ms_pe_clang_gas.S"));
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/ontop_x86_64_ms_pe_clang_gas.S"));
-            },
-            .macos, .ios, .watchos, .tvos, .visionos => {
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/jump_x86_64_sysv_macho_gas.S"));
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/make_x86_64_sysv_macho_gas.S"));
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/ontop_x86_64_sysv_macho_gas.S"));
-            },
-            else => {
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/jump_x86_64_sysv_elf_gas.S"));
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/make_x86_64_sysv_elf_gas.S"));
-                context.root_module.addAssemblyFile(context_path.path(b, "asm/ontop_x86_64_sysv_elf_gas.S"));
-            },
-        },
-        else => @panic("Invalid arch"),
-    }
 
     const module = b.addModule("fimo_tasks", .{
         .root_source_file = b.path("src/root.zig"),
@@ -68,11 +23,47 @@ pub fn configure(builder: *build_internals.FimoBuild) void {
     });
     module.addImport("fimo_std", fimo_std_pkg.root_module);
     module.addImport("fimo_tasks_meta", fimo_tasks_meta_pkg.root_module);
-    module.linkLibrary(context);
     if (builder.graph.target.result.os.tag == .windows) {
         if (b.lazyDependency("win32", .{})) |dep| {
             module.addImport("win32", dep.module("win32"));
         }
+    }
+    switch (builder.graph.target.result.cpu.arch) {
+        .aarch64 => switch (builder.graph.target.result.os.tag) {
+            .windows => {
+                if (builder.graph.target.result.abi == .msvc) {
+                    module.addAssemblyFile(context_path.path(b, "asm/jump_arm64_aapcs_pe_armasm.asm"));
+                    module.addAssemblyFile(context_path.path(b, "asm/ontop_arm64_aapcs_pe_armasm.asm"));
+                }
+            },
+            .macos, .ios, .watchos, .tvos, .visionos => {
+                module.addAssemblyFile(context_path.path(b, "asm/jump_arm64_aapcs_macho_gas.S"));
+                module.addAssemblyFile(context_path.path(b, "asm/make_arm64_aapcs_macho_gas.S"));
+                module.addAssemblyFile(context_path.path(b, "asm/ontop_arm64_aapcs_macho_gas.S"));
+            },
+            else => {
+                module.addAssemblyFile(context_path.path(b, "asm/jump_arm64_aapcs_elf_gas.S"));
+                module.addAssemblyFile(context_path.path(b, "asm/make_arm64_aapcs_elf_gas.S"));
+                module.addAssemblyFile(context_path.path(b, "asm/ontop_arm64_aapcs_elf_gas.S"));
+            },
+        },
+        .x86_64 => switch (builder.graph.target.result.os.tag) {
+            .windows => {
+                module.addAssemblyFile(context_path.path(b, "asm/jump_x86_64_ms_pe_clang_gas.S"));
+                module.addAssemblyFile(context_path.path(b, "asm/ontop_x86_64_ms_pe_clang_gas.S"));
+            },
+            .macos, .ios, .watchos, .tvos, .visionos => {
+                module.addAssemblyFile(context_path.path(b, "asm/jump_x86_64_sysv_macho_gas.S"));
+                module.addAssemblyFile(context_path.path(b, "asm/make_x86_64_sysv_macho_gas.S"));
+                module.addAssemblyFile(context_path.path(b, "asm/ontop_x86_64_sysv_macho_gas.S"));
+            },
+            else => {
+                module.addAssemblyFile(context_path.path(b, "asm/jump_x86_64_sysv_elf_gas.S"));
+                module.addAssemblyFile(context_path.path(b, "asm/make_x86_64_sysv_elf_gas.S"));
+                module.addAssemblyFile(context_path.path(b, "asm/ontop_x86_64_sysv_elf_gas.S"));
+            },
+        },
+        else => @panic("Invalid arch"),
     }
 
     const mod = builder.addModule(.{
@@ -92,11 +83,47 @@ pub fn configure(builder: *build_internals.FimoBuild) void {
                 });
                 t.addImport("fimo_std", fimo_std_pkg.root_module);
                 t.addImport("fimo_tasks_meta", fimo_tasks_meta_pkg.root_module);
-                t.linkLibrary(context);
                 if (builder.graph.target.result.os.tag == .windows) {
                     if (b.lazyDependency("win32", .{})) |dep| {
                         t.addImport("win32", dep.module("win32"));
                     }
+                }
+                switch (builder.graph.target.result.cpu.arch) {
+                    .aarch64 => switch (builder.graph.target.result.os.tag) {
+                        .windows => {
+                            if (builder.graph.target.result.abi == .msvc) {
+                                t.addAssemblyFile(context_path.path(b, "asm/jump_arm64_aapcs_pe_armasm.asm"));
+                                t.addAssemblyFile(context_path.path(b, "asm/ontop_arm64_aapcs_pe_armasm.asm"));
+                            }
+                        },
+                        .macos, .ios, .watchos, .tvos, .visionos => {
+                            t.addAssemblyFile(context_path.path(b, "asm/jump_arm64_aapcs_macho_gas.S"));
+                            t.addAssemblyFile(context_path.path(b, "asm/make_arm64_aapcs_macho_gas.S"));
+                            t.addAssemblyFile(context_path.path(b, "asm/ontop_arm64_aapcs_macho_gas.S"));
+                        },
+                        else => {
+                            t.addAssemblyFile(context_path.path(b, "asm/jump_arm64_aapcs_elf_gas.S"));
+                            t.addAssemblyFile(context_path.path(b, "asm/make_arm64_aapcs_elf_gas.S"));
+                            t.addAssemblyFile(context_path.path(b, "asm/ontop_arm64_aapcs_elf_gas.S"));
+                        },
+                    },
+                    .x86_64 => switch (builder.graph.target.result.os.tag) {
+                        .windows => {
+                            t.addAssemblyFile(context_path.path(b, "asm/jump_x86_64_ms_pe_clang_gas.S"));
+                            t.addAssemblyFile(context_path.path(b, "asm/ontop_x86_64_ms_pe_clang_gas.S"));
+                        },
+                        .macos, .ios, .watchos, .tvos, .visionos => {
+                            t.addAssemblyFile(context_path.path(b, "asm/jump_x86_64_sysv_macho_gas.S"));
+                            t.addAssemblyFile(context_path.path(b, "asm/make_x86_64_sysv_macho_gas.S"));
+                            t.addAssemblyFile(context_path.path(b, "asm/ontop_x86_64_sysv_macho_gas.S"));
+                        },
+                        else => {
+                            t.addAssemblyFile(context_path.path(b, "asm/jump_x86_64_sysv_elf_gas.S"));
+                            t.addAssemblyFile(context_path.path(b, "asm/make_x86_64_sysv_elf_gas.S"));
+                            t.addAssemblyFile(context_path.path(b, "asm/ontop_x86_64_sysv_elf_gas.S"));
+                        },
+                    },
+                    else => @panic("Invalid arch"),
                 }
 
                 break :blk t;
